17c6e9d65031ccf81ceb3643caa0a1d6bacf8856
angie-kinya <angiekinya670@gmail.com>
2025-01-03 12:07:35 +0300

    Filtering by Regions
    Added a feature that enables one to filter the cities by region.

--- weather_scope/weather/models.py
+++ weather_scope/weather/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class WeatherQuery(models.Model):
     city = models.CharField(max_length=100)
+    region = models.CharField(max_length=100, blank=True) # Kenyan counties or cities
     query_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.city
+        return f"{self.city}, {self.region}"
--- weather_scope/weather/views.py
+++ weather_scope/weather/views.py
@@ -1,18 +1,39 @@
 from django.shortcuts import render
 from .services import fetch_weather_data
 from .models import WeatherQuery
+from django.utils.timezone import now
+import pytz
+from django.db.models import Q
 
 def weather_dashboard(request):
     weather_info = None
+    filters = {}
     if request.method == "POST":
         city = request.POST.get("city")
+        region = request.POST.get("region", "")
         weather_data = fetch_weather_data(city)
         if weather_data:
+            query_time = now().astimezone(pytz.timezone("Africa/Nairobi"))
             weather_info = {
                 "city": city,
+                "region": region,
                 "temperature": weather_data["main"]["temp"],
                 "description": weather_data["weather"][0]["description"],
                 "icon": weather_data["weather"][0]["icon"],
+                "date": query_time.strftime("%A, %d %B %Y"),
+                "time": query_time.strftime("%I:%M %p"),
             }
-            WeatherQuery.objects.create(city=city)
-    return render(request, "weather/dashboard.html", {"weather_info": weather_info})
+            WeatherQuery.objects.create(city=city, region=region)
+
+    # Filtering by regions
+    if request.GET.get("filter_date"):
+        filters["query_time__date"] = request.GET.get("filter_date")
+    if request.GET.get("filter_region"):
+        filters["region__icontains"] = request.GET.get("filter_region")
+
+    filtered_queries = WeatherQuery.objects.filter(**filters).order_by("-query_time")
+
+    return render(request, "weather/dashboard.html", {
+        "weather_info": weather_info, 
+        "filtered_queries": filtered_queries
+    })  # noqa: E501
